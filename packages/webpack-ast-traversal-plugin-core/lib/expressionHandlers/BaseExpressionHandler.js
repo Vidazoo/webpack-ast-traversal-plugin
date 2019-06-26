8382783e481848a87c209a26b9ead0bf19c72b05
Udi Talias <udi.talias@gmail.com>
2019-06-26 17:51:22 +0300

    feat(module): base abstract `apply` method

--- packages/webpack-ast-traversal-plugin-core/lib/expressionHandlers/BaseExpressionHandler.js
+++ packages/webpack-ast-traversal-plugin-core/lib/expressionHandlers/BaseExpressionHandler.js
@@ -1,4 +1,9 @@
 class BaseExpressionHandler {
+    
+    apply(compiler) {
+        throw new TypeError(`[${this.constructor.name}] apply method not implemented.`);
+    }
+
     shouldIgnoreNodeByComment(node, commentBlock) {
         if (node.comments) {
             return !!node.comments.find((comment) => {
